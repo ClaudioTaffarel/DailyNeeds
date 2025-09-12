e67c46121cc66cf215a824a5f0727d2c6ae89813
ClaudioTaffarel <claudio.taffarel889@gmail.com>
2025-09-12 21:50:33 +0700

    Commit 5 Claudio: Revisi Design

--- scripts/WalkSimu.js
+++ scripts/WalkSimu.js
@@ -6,45 +6,42 @@ let resetBtn = document.getElementById('resetBtn');
 let messagezz = document.getElementById('messagezz');
 
 let pos = 0;
-let direction = 1; // 1 = kanan, -1 = kiri
-const stepSize = 50; // pixel per langkah
-const stepValue = 100; // 100 steps tiap klik
+let direction = 1;
+const stepSize = 50;
+const stepValue = 100;
 
-// Walk button
 walkBtn.addEventListener('click', () => {
-  steps += stepValue;
+    steps += stepValue;
 
-  pos += stepSize * direction;
-  if (pos >= window.innerWidth - 120) {
-    direction = -1;
-    person.style.transform = 'scaleX(-1)';
-  } else if (pos <= 0) {
-    direction = 1;
-    person.style.transform = 'scaleX(1)';
-  }
-  person.style.left = pos + 'px';
+    pos += stepSize * direction;
+    if (pos >= window.innerWidth - 120) {
+        direction = -1;
+        person.style.transform = 'scaleX(-1)';
+    } else if (pos <= 0) {
+        direction = 1;
+        person.style.transform = 'scaleX(1)';
+    }
+    person.style.left = pos + 'px';
 
-  messagezz.textContent = "Steps: " + steps;
+    messagezz.textContent = 'Steps: ' + steps;
 });
 
-// Stop button (pakai if else)
 stopBtn.addEventListener('click', () => {
-  person.style.display = "none";
+    person.style.display = 'none';
 
-  if (steps < 1000) {
-    messagezz.textContent = `You've walked ${steps} steps! This is not yet ideal. Based on health recommendations, humans should walk at least 1000 steps a day.`;
-  } else {
-    messagezz.textContent = `Congrats! You've walked ${steps} steps! This is ideal based on health recommendations — keep it up!`;
-  }
+    if (steps < 1000) {
+        messagezz.textContent = `You've walked ${steps} steps! This is not yet ideal. Based on health recommendations, humans should walk at least 1000 steps a day.`;
+    } else {
+        messagezz.textContent = `Congrats! You've walked ${steps} steps! This is ideal based on health recommendations — keep it up!`;
+    }
 });
 
-// Reset button
 resetBtn.addEventListener('click', () => {
-  person.style.display = "block";
-  steps = 0;
-  pos = 0;
-  direction = 1;
-  person.style.left = pos + 'px';
-  person.style.transform = 'scaleX(1)';
-  messagezz.textContent = "Steps reset to 0!";
+    person.style.display = 'block';
+    steps = 0;
+    pos = 0;
+    direction = 1;
+    person.style.left = pos + 'px';
+    person.style.transform = 'scaleX(1)';
+    messagezz.textContent = 'Steps reset to 0!';
 });
